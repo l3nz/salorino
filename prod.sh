e68fa3105b1515074f54a0dd3190cb2848af0bae
lenz <lenz@mac>
2019-02-16 17:52:17 +0100

    Fixing #8 - HTML generato e titoli

--- prod.sh
+++ prod.sh
@@ -1,7 +1,22 @@
-cd sito &&
+cd sito 
+
 bundle config build.nokogiri --use-system-libraries \
   --with-xml2-include=$(brew --prefix libxml2)/include/libxml2 &&
 bundle install --path ~/_vendor/bundle &&
 bundle exec jekyll build --config "_config.yml" -d "_UPLOAD_ME" 
+
+# Rimpiazza il contenuto di <title>...</title>
+INDEX="./_UPLOAD_ME/index.html"
+
+sed -i '' \
+   's%<title>.\{1,\}<\/title>%<title>Parrocchia di Salorino</title>%' \
+   $INDEX
+
+find ./_UPLOAD_ME -type f -name "index.html" -exec \
+	sed -i '' \
+   		's%Powered by .\{1,\}.<\/div>%<\/div>%' \
+   		{} \;
+
+
 
 
