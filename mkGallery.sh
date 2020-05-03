7a716be2172eb385903c81f3c3724e121448c129
lenz <lenz@mac>
2020-05-03 15:14:09 +0200

    Tool per creazione gallerie

--- mkGallery.sh
+++ mkGallery.sh
@@ -1,17 +1,54 @@
 #! /bin/bash
 
-GALLERY="2019/lanternata"
+DATA=2020-04-30
+TITOLO=entrata-don-maksym
+GALLERY="2017/don-maksym"
+BASE=$(pwd)
 
+POST=${BASE}/sito/_posts/${DATA}-${TITOLO}.md
 LOCAL=./sito/assets/images/${GALLERY}
+
+#
+# Prefisso
+# 
+
+
+
+echo "\
+---
+layout: single
+title:  \"${TITOLO}\"
+date:   ${DATA} 16:56:47 +0200
+categories: Foto
+tags: Comunità Confraternita
+gallery: 
+" >${POST}
+
 
 cd $LOCAL
 
-echo "gallery:"
+
+
+
+
 for f in *.jpg
 do
-	echo "  - url: \"/assets/images/${GALLERY}/${f}\""
-	echo "    image_path: \"/assets/images/${GALLERY}/${f}\""
+	echo "  - url: \"/assets/images/${GALLERY}/${f}\""  >>${POST}
+	echo "    image_path: \"/assets/images/${GALLERY}/${f}\""  >>${POST}
 done
 
 
+
+
+echo "\
+---
+
+Alcune immagini ${TITOLO}, che si è tenuta il ${DATA}.
+
+
+
+
+{% include gallery caption=\"Grazie a tutti per la partecipazione!\" %}
+
+" >>${POST}
 
