12eaa1bcdb96609793839a3d77136f48b1af0f09
Lenz <lenz@home>
2023-01-22 16:22:18 +0100

    Usa porta 4099 per test locale

--- runLocal.sh
+++ runLocal.sh
@@ -3,6 +3,6 @@ bundle config force_ruby_platform true && \
 bundle config build.nokogiri --platform arm64-darwin --use-system-libraries \
   --with-xml2-include=$(brew --prefix libxml2)/include/libxml2 &&
 bundle install --path ~/.bundle &&
-bundle exec jekyll serve --config "_config.yml,_config_local.yml"  -w
+bundle exec jekyll serve -P 4099 --config "_config.yml,_config_local.yml"  -w
 
 
